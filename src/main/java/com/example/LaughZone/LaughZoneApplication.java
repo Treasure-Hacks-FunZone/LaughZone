6267748c4095a1fa757b45b8b28eb6d790de09d8
= <=>
2023-04-28 20:42:49 -0400

    Starting point

---
+++ src/main/java/com/example/LaughZone/LaughZoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.LaughZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaughZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaughZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LaughZone/LaughZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LaughZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaughZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
